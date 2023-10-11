d37d9873e33ac962ae6b6319462e9f60daa69f38
WaQw <47028829+WaQw@users.noreply.github.com>
2023-10-11 13:57:49 -0400

    Init

---
+++ index.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+////////////////////////////
+// Files
+
+// Blocking, synchronous way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn)
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File written!');
+
+// Non-blocking, asynchronous way
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//     if(err) return console.log("Error!")
+
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2);
+//         fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//             console.log(data3);
+//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
+//                 console.log("Your file has been written!")
+//             })
+//         });
+//     });
+// });
+// console.log("Will read file!")
+
+////////////////////////////
+// Server
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+// each time a new request hits the server, the callback function is called
+// e.g. a request can be accessing the ip:port on browser
+const server = http.createServer((req, res) => {
+    // Routing
+    const pathName = req.url;
+    if(pathName === '/' || pathName === '/overview') {
+        res.end('This is the OVERVIEW');
+    } else if(pathName === '/product') {
+        res.end('This is the PRODUCT');
+    } else if(pathName === '/api') {
+        res.writeHead(200, {
+            'Content-type': 'application/json', // the browser is now expecting json
+        });
+        res.end(data);
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html', // the browser is now expecting html
+            'my-own-header': 'angela-created'
+        });
+        res.end('<h1>Page not found!</h1>');
+    }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000');
+});
+
